f5df10ab02b135527d0462f81043dbc3585de154
LucasSoto02 <lucasgsotto@gmail.com>
2024-03-08 14:13:51 -0300

    initial commit

---
+++ script/script.js
@@ -0,0 +1,57 @@
+/*
+String
+""
+''
+``
+
+Number 
+1 - integer
+1.2 - float
+
+Boolean
+true
+false 
+
+Array
+[]
+[1,2,3]
+
+*/
+
+/* function sayHello(name){
+    alert("Olá " +  name)
+}
+
+sayHello("Lucas")
+*/
+/*
+function sum(a,b) {
+    return a * b - metodo
+}
+
+sum(3,5) - atributos enviados para a função
+
+*/
+
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle('light')
+    /*if(html.classList.contains('light')) {
+        html.classList.remove('light')
+    }else{
+        html.classList.add('light')
+    }
+    */
+}
+
+/*
+git init - inicia o git
+
+git add . - adiciona todos os arquivos modificados ao stage
+
+git commit -m "message here" - cria e descreve um ponto 
+
+git push - envia as alterações pro repositório
+
+
+*/
